61008a0b35720b02ab9640812d34df9da0ac8a02
Roshan Lakmal Elvitigala <roshanrox911@gmail.com>
2015-07-02 00:38:12 +0530

    Fix issues in Sign Up and add licence

--- app/src/main/java/app/android/box/waveprotocol/org/androidwave/SignUpActivity.java
+++ app/src/main/java/app/android/box/waveprotocol/org/androidwave/SignUpActivity.java
@@ -1,3 +1,22 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 package app.android.box.waveprotocol.org.androidwave;
 
 import android.app.Activity;
@@ -21,7 +40,8 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 
 /**
- * Created by roshan on 6/24/15.
+ *  Apache Wave Sign Up Activity
+ *  Created by roshan on 6/24/15.
  */
 public class SignUpActivity extends Activity {
 
@@ -64,9 +84,7 @@ public class SignUpActivity extends Activity {
         signUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                waveSignUpTask.execute(getHostName(username.getText().toString()), username.getText().toString(), password.getText().toString());
-                Intent openInboxActivity = new Intent("app.android.box.waveprotocol.org.androidwave.INBOXACTIVITY");
-                startActivity(openInboxActivity);
+                waveSignUpTask.execute(getHostAndUserNames(username.getText().toString())[0], getHostAndUserNames(username.getText().toString())[0], password.getText().toString());
             }
         });
 
@@ -79,6 +97,16 @@ public class SignUpActivity extends Activity {
         });
     }
 
+
+    /**
+     * This method get Wave server name, Wave user's username and Wave user's password as input parameters
+     * and it will invoke UserRegistrationServlet in the Wave server. If sign up get success the method
+     * will return true if not it return false
+     * @param host Apache Wave hostname
+     * @param username Apache Wave user's username
+     * @param password Apache Wave user's password
+     * @return True or false
+     */
     private boolean waveSignUp(String host, String username, String password) {
 
         String hostURL = hostCreator(host);
@@ -118,22 +146,30 @@ public class SignUpActivity extends Activity {
             connection.disconnect();
         }
 
-        return true;
+        return false;
     }
 
-    private String getHostName(String username) {
-
-        String hostName = "";
+    /**
+     * This method get Wave username as input and it split it to username and hostname
+     * @param username Wave user's username
+     * @return hostname and username
+     */
+    private String[] getHostAndUserNames(String username) {
 
         String[] usernameAndHost = username.split("@");
 
         if (usernameAndHost.length > 1) {
-            return usernameAndHost[1];
+            return usernameAndHost;
         }
 
-        return hostName;
+        return null;
     }
 
+    /**
+     * This method generate http url for given hostname
+     * @param hostname Apache Wave server name
+     * @return http url of the host
+     */
     private String hostCreator(String hostname) {
 
         StringBuilder hostUrl = new StringBuilder();
@@ -146,13 +182,5 @@ public class SignUpActivity extends Activity {
 
         return hostUrl.append("http://").append(hostname).toString();
     }
-
-//    private String httpQueryCreator(String host, String userName, String password){
-//
-//        StringBuilder httpQuery = new StringBuilder();
-//
-//
-//
-//    }
 
 }
