da607a9d630e2334f4a3155f22def91f40e666c8
TinTin911 <roshanrox911@gmail.com>
2015-07-03 22:15:29 +0530

    Added the search icon

--- app/src/main/java/app/android/box/waveprotocol/org/androidwave/InboxActivity.java
+++ app/src/main/java/app/android/box/waveprotocol/org/androidwave/InboxActivity.java
@@ -19,16 +19,46 @@
 
 package app.android.box.waveprotocol.org.androidwave;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 /**
  * Created by roshan on 6/25/15.
  */
-public class InboxActivity extends Activity{
+public class InboxActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inbox);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
     }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+     /*
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    */
 }
