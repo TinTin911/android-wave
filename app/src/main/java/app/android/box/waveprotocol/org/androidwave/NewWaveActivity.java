120a67b1c7de0626607dd84c0f21c984c91aee4e
TinTin911 <roshanrox911@gmail.com>
2015-07-26 10:55:12 +0530

    Updating the slider

--- app/src/main/java/app/android/box/waveprotocol/org/androidwave/NewWaveActivity.java
+++ app/src/main/java/app/android/box/waveprotocol/org/androidwave/NewWaveActivity.java
@@ -1,15 +1,28 @@
 package app.android.box.waveprotocol.org.androidwave;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 
 /**
  * Created by roshan on 7/12/15.
  */
-public class NewWaveActivity extends Activity{
+public class NewWaveActivity extends AppCompatActivity{
+    private Toolbar toolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_wave);
+
+        toolbar = (Toolbar) findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
