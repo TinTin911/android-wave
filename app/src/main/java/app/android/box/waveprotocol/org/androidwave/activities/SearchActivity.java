d7127ce2404021d6c248cd29f44c5288944982cc
unknown <roellk@ROELLK1.corpnet.ifsworld.com>
2015-08-09 16:31:16 +0530

    Adding the search activity

---
+++ app/src/main/java/app/android/box/waveprotocol/org/androidwave/activities/SearchActivity.java
@@ -0,0 +1,17 @@
+package app.android.box.waveprotocol.org.androidwave.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import app.android.box.waveprotocol.org.androidwave.R;
+
+/**
+ * Created by roellk on 8/9/2015.
+ */
+public class SearchActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+    }
+}
