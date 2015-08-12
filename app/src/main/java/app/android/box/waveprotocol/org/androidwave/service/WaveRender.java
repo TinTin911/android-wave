c96734bf017117e48d72dc74a89f7b65f722436e
TinTin911 <roshanrox911@gmail.com>
2015-08-12 15:37:43 +0530

    Create wave (new wave) is done

--- app/src/main/java/app/android/box/waveprotocol/org/androidwave/service/WaveRender.java
+++ app/src/main/java/app/android/box/waveprotocol/org/androidwave/service/WaveRender.java
@@ -1,15 +1,32 @@
 package app.android.box.waveprotocol.org.androidwave.service;
 
+import com.google.common.base.Preconditions;
+
 import org.waveprotocol.wave.concurrencycontrol.common.UnsavedDataListener;
+import org.waveprotocol.wave.concurrencycontrol.wave.CcDataDocumentImpl;
 import org.waveprotocol.wave.model.conversation.ObservableConversationView;
 import org.waveprotocol.wave.model.conversation.WaveBasedConversationView;
 import org.waveprotocol.wave.model.document.WaveContext;
+import org.waveprotocol.wave.model.document.indexed.IndexedDocumentImpl;
+import org.waveprotocol.wave.model.document.operation.DocInitialization;
+import org.waveprotocol.wave.model.document.operation.automaton.DocumentSchema;
 import org.waveprotocol.wave.model.id.IdGenerator;
+import org.waveprotocol.wave.model.id.WaveId;
+import org.waveprotocol.wave.model.id.WaveletId;
+import org.waveprotocol.wave.model.schema.SchemaProvider;
+import org.waveprotocol.wave.model.schema.conversation.ConversationSchemas;
+import org.waveprotocol.wave.model.version.HashedVersion;
 import org.waveprotocol.wave.model.wave.ParticipantId;
+import org.waveprotocol.wave.model.wave.data.ObservableWaveletData;
 import org.waveprotocol.wave.model.wave.data.WaveViewData;
+import org.waveprotocol.wave.model.wave.data.impl.ObservablePluggableMutableDocument;
 import org.waveprotocol.wave.model.wave.data.impl.WaveViewDataImpl;
+import org.waveprotocol.wave.model.wave.data.impl.WaveletDataImpl;
+import org.waveprotocol.wave.model.wave.data.DocumentFactory;
 import org.waveprotocol.wave.model.wave.opbased.OpBasedWavelet;
 import org.waveprotocol.wave.model.wave.opbased.WaveViewImpl;
+import org.waveprotocol.wave.model.wave.opbased.WaveViewImpl.WaveletFactory;
+import org.waveprotocol.wave.model.wave.opbased.WaveViewImpl.WaveletConfigurator;
 import org.waveprotocol.wave.model.waveref.WaveRef;
 
 import java.util.Set;
@@ -17,6 +34,8 @@ import java.util.Timer;
 
 import app.android.box.waveprotocol.org.androidwave.service.concurrencycontrol.MuxConnector;
 import app.android.box.waveprotocol.org.androidwave.service.concurrencycontrol.MuxConnector.Command;
+import app.android.box.waveprotocol.org.androidwave.service.concurrencycontrol.WaveletOperationalizer;
+import app.android.box.waveprotocol.org.androidwave.service.documents.WaveDocuments;
 
 public class WaveRender {
 
@@ -35,15 +54,17 @@ public class WaveRender {
 
     private ObservableConversationView conversations;
     private WaveViewImpl<OpBasedWavelet> wave;
+    private WaveletOperationalizer wavelets;
+    private WaveDocuments<CcDataDocumentImpl> documentRegistry;
 
     private boolean isClosed = true;
     private Timer timer;
 
 
     public WaveRender(boolean isNewWave, WaveRef waveRef, RemoteViewServiceMultiplexer waveChannel,
-                         ParticipantId waveParticipant,
-                         Set<ParticipantId> waveOtherParticipants, IdGenerator waveIdGenerator,
-                         UnsavedDataListener unsavedDataListener, Timer timer) {
+                      ParticipantId waveParticipant,
+                      Set<ParticipantId> waveOtherParticipants, IdGenerator waveIdGenerator,
+                      UnsavedDataListener unsavedDataListener, Timer timer) {
         this.signedInuser = waveParticipant;
         this.waveRef = waveRef;
         this.isNewWave = isNewWave;
@@ -101,12 +122,74 @@ public class WaveRender {
         return waveContext;
     }
 
+    private WaveViewImpl<OpBasedWavelet> createWave() {
+
+        WaveViewData snapshot = getWaveData();
+
+        final WaveletOperationalizer operationalizer = getWavelets();
+        WaveletFactory<OpBasedWavelet> waveletFactory = new WaveletFactory<OpBasedWavelet>() {
+            @Override
+            public OpBasedWavelet create(WaveId waveId, WaveletId id, ParticipantId creator) {
+                long now = System.currentTimeMillis();
+                ObservableWaveletData data = new WaveletDataImpl(id, creator, now, 0L,
+                        HashedVersion.unsigned(0), now, waveId, getDocumentRegistry());
+                return operationalizer.operationalize(data);
+            }
+        };
+        WaveViewImpl<OpBasedWavelet> wave = WaveViewImpl.create(waveletFactory, snapshot.getWaveId(),
+                getIdGenerator(), getSignedInUser(), WaveletConfigurator.ADD_CREATOR);
+
+        for (ObservableWaveletData waveletData : snapshot.getWavelets()) {
+            wave.addWavelet(operationalizer.operationalize(waveletData));
+        }
+        return wave;
+    }
+
+    private WaveViewData getWaveData() {
+        Preconditions.checkState(waveData != null, "wave not ready");
+        return waveData;
+    }
+
+    private WaveletOperationalizer getWavelets() {
+        return wavelets == null ? wavelets = createWavelets() : wavelets;
+    }
+
+    private WaveletOperationalizer createWavelets() {
+        return WaveletOperationalizer.create(getWaveData().getWaveId(), getSignedInUser());
+    }
+
+    private ParticipantId getSignedInUser() {
+        return signedInuser;
+    }
+
+    private WaveDocuments<CcDataDocumentImpl> getDocumentRegistry() {
+        return documentRegistry == null
+                ? documentRegistry = createDocumentRegistry() : documentRegistry;
+    }
+
+    private WaveDocuments<CcDataDocumentImpl> createDocumentRegistry() {
+        IndexedDocumentImpl.performValidation = false;
+
+        DocumentFactory<?> dataDocFactory =
+                ObservablePluggableMutableDocument.createFactory(createSchemas());
+
+        DocumentFactory<CcDataDocumentImpl> fakeBlipDocFactory = new DocumentFactory<CcDataDocumentImpl>() {
+
+            @Override
+            public CcDataDocumentImpl create(WaveletId waveletId, String docId, DocInitialization content) {
+                return new CcDataDocumentImpl(DocumentSchema.NO_SCHEMA_CONSTRAINTS, content);
+            }
+
+        };
+
+        return WaveDocuments.create(fakeBlipDocFactory, dataDocFactory);
+    }
+
+    private SchemaProvider createSchemas() {
+        return new ConversationSchemas();
+    }
+
     private MuxConnector createConnector() {
         return null;
     }
-
-    private WaveViewImpl<OpBasedWavelet> createWave() {
-        return null;
-    }
-
 }
