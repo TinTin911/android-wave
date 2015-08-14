192c619960cc14f64e7ad4dc1ccd818eaaa14c13
TinTin911 <roshanrox911@gmail.com>
2015-08-14 10:29:02 +0530

    Adding the front button to the search

--- app/src/main/java/app/android/box/waveprotocol/org/androidwave/activities/NewWaveActivity.java
+++ app/src/main/java/app/android/box/waveprotocol/org/androidwave/activities/NewWaveActivity.java
@@ -19,6 +19,7 @@
 
 package app.android.box.waveprotocol.org.androidwave.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -26,6 +27,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ImageView;
 
 import app.android.box.waveprotocol.org.androidwave.R;
 
@@ -45,6 +48,7 @@ public class NewWaveActivity extends AppCompatActivity{
         Fragment fragment = null;
         String title = getString(R.string.app_name);
 
+
                 fragment = new NewWaveFragment();
                 title = getString(R.string.new_wave_name);
 
@@ -57,6 +61,28 @@ public class NewWaveActivity extends AppCompatActivity{
             // set the toolbar title
             getSupportActionBar().setTitle(title);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 //
