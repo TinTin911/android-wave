bab601d6fc701c0aa1dc704fdcea049c2d80dee4
TinTin911 <roshanrox911@gmail.com>
2015-08-19 11:15:41 +0530

    Adding the MessageAdapter

--- app/src/main/java/app/android/box/waveprotocol/org/androidwave/activities/InboxFragment.java
+++ app/src/main/java/app/android/box/waveprotocol/org/androidwave/activities/InboxFragment.java
@@ -19,7 +19,7 @@ import app.android.box.waveprotocol.org.androidwave.R;
 
 public class InboxFragment extends ListFragment {
 
-
+    MessageAdapter myadapter;
 
     public InboxFragment() {
         // Required empty public constructor
@@ -29,12 +29,15 @@ public class InboxFragment extends ListFragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String[] values = new String[] { "Message1", "Message2", "Message3",
-                "Message4", "Message5", "Message6", "Message7", "Message8",
-                "Message9", "Message10" };
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
-                android.R.layout.simple_list_item_1, values);
-        setListAdapter(adapter);
+//        String[] values = new String[] { "Message1", "Message2", "Message3",
+//                "Message4", "Message5", "Message6", "Message7", "Message8",
+//                "Message9", "Message10" };
+//
+//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(),
+//                android.R.layout.simple_list_item_1, values);
+//        setListAdapter(adapter);
+        myadapter = new MessageAdapter(getActivity());
+        setListAdapter(myadapter);
 
     }
 
