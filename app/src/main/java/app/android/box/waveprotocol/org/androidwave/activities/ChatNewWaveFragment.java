25143f245765008f520586e2cc3bc38fbaf0a2af
TinTin911 <roshanrox911@gmail.com>
2015-08-28 08:48:11 +0530

    Updating the activity and fragment

--- app/src/main/java/app/android/box/waveprotocol/org/androidwave/activities/ChatNewWaveFragment.java
+++ app/src/main/java/app/android/box/waveprotocol/org/androidwave/activities/ChatNewWaveFragment.java
@@ -1,7 +1,10 @@
 package app.android.box.waveprotocol.org.androidwave.activities;
 
+import android.app.Fragment;
+
 /**
  * Created by roellk on 8/24/2015.
  */
-public class ChatNewWaveFragment {
+public class ChatNewWaveFragment extends Fragment{
+
 }
